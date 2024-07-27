d4d2990f98b265f4ddb89760230b6bdd106fa8bd
mimihua <68930310+mimihua@users.noreply.github.com>
2024-07-27 14:38:32 +0900

    update

--- functions/eslint.config.mjs
+++ functions/eslint.config.mjs
@@ -1,12 +1,46 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import importPlugin from "eslint-plugin-import";
+import googleConfig from "eslint-config-google";
+import pkg from '@eslint/js';
+const { configs: eslintConfigs } = pkg;
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ["tsconfig.json", "tsconfig.dev.json"],
+        sourceType: "module",
+      },
+      globals: globals.node,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      "import": importPlugin,
+    },   
+    rules: {
+      ...eslintConfigs.recommended.rules,
+      ...importPlugin.configs.errors.rules,
+      ...importPlugin.configs.warnings.rules,
+      ...importPlugin.configs.typescript.rules,
+      // ↓↓↓googleConfig.rules↓↓↓
+      ...googleConfig.rules,
+      "valid-jsdoc": "off", // 禁用弃用的规则
+      "require-jsdoc": "off", // 禁用不可用的规则
+      // ↑↑↑googleConfig.rules↑↑↑   
+      ...tseslint.configs.recommended.rules,
+      "quotes": ["error", "double"],
+      "import/no-unresolved": 0,
+      "indent": ["error", 2],
+      "linebreak-style": ["error", "unix"] // 确保使用 Unix 风格的换行符
+    },
+    ignores: [
+      "/lib/**/*", // Ignore built files.
+      "/generated/**/*", // Ignore generated files.
+    ],
+  }
 ];
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,15 +9,13 @@
 
 import * as logger from "firebase-functions/logger";
 
-import { onSchedule } from "firebase-functions/v2/scheduler";
-import { helloworld } from "./puppeteer_script";
+import {onSchedule} from "firebase-functions/v2/scheduler";
+import {helloworld} from "./puppeteer_script";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-
 export const scheduledFunction = onSchedule("* * * * *", async () => {
-
   logger.log("Schedule function triggered!");
   // Fetch all user details.
   await helloworld();
